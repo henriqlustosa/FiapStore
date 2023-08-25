9b93b9ac5acf53bc3649980a7938b951d8012ab5
henriqlustosa <henriqlustosa@gmail.com>
2023-08-25 17:39:26 -0300

    Criado o controller do usuario com os comandos do CRUD.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FiapStore.Controllers
+{
+    [ApiController]
+    [Route("Usuario")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("obter-todos-usuarios")]
+        public IActionResult ObterTodosUsuarios()
+        {
+            return Ok("Usuários obtidos com sucesso!");
+        }
+
+        [HttpGet("obter-usuario-por-id")]
+        public IActionResult ObterUsuarioPorId()
+        {
+            return Ok("Usuário obtidos com sucesso!");
+        }
+        [HttpPost]
+        public IActionResult CriarUsuario()
+        {
+            return Ok("Usuário adicionado com sucesso!");
+        }
+
+        [HttpPut]
+        public IActionResult EditarUsuario()
+        {
+            return Ok("Usuário atualizado com sucesso!");
+        }
+
+        [HttpDelete]
+        public IActionResult RemoverUsuario()
+        {
+            return Ok("Usuário removido com sucesso!");
+        }
+    }
+}
