176f6eca499d6155830ed99c8f081de1ed78419e
henriqlustosa <henriqlustosa@gmail.com>
2023-08-31 17:53:33 -0300

    Inicio da implementacao das classes de para persistencia de dados

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using FiapStore.Interface;
 using FiapStore.Repository;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
---
+++ Repository/DapperRepository.cs
@@ -0,0 +1,41 @@
+﻿using FiapStore.Entity;
+using FiapStore.Interface;
+
+namespace FiapStore.Repository
+{
+    public abstract class DapperRepository<T> : IRepository<T> where T : Entidade
+    {
+        private readonly string _connectionString;
+
+        protected string ConnectionString => _connectionString;
+
+        public DapperRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetValue<string>("ConnectionStrings:ConnectionString");
+        }
+        public void Alterar(T entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(T entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<T> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
